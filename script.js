c693ee8fbeab9a5307c84509eed00efe42e25993
webOps-aman <amanhxe@gmail.com>
2025-03-14 19:34:28 +0530

    added api key

---
+++ script.js
@@ -0,0 +1,27 @@
+import accessKey from "./config.js";
+
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+
+    // const results
+}
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
